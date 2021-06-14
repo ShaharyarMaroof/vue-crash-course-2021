2393b40c6b697de79eb8f1abd7ec05af82633d84
Shaharyar Maroof <shaharyar.maroof@emumba.com>
2021-06-14 16:20:11 +0500

    added task creation form

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClickHandler()" :style="{ background: color }" class="btn">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: String,
+    color: String,
+    // onClickHandler: {
+    //   type: Function,
+    //   default: () => console.log(`no handler attached ${this.props.label}`),
+    // },
+  },
+  methods: {
+    onClickHandler() {
+      console.log(`button clicked`);
+    },
+  },
+};
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,41 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder', id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <i @click="$emit('delete-task', id)" class="fas fa-times" />
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  emits:["delete-task", "toggle-reminder"]
+};
+</script>
+
+<style scoped lang="scss">
+.fas {
+  color: red;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+
+  h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &.reminder {
+    border-left: 5px solid green;
+  }
+}
+</style>
